9f1a288e68263e05245ecf12292f3c5703232cc7
iamtylerd <tylerdanielphotography@gmail.com>
2016-09-26 11:58:13 -0500

    added mongodb

--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,25 @@
 'use strict';
 
 angular
-	.module('mean101', [])
-	.controller('main', function ($scope, $http) {
+	.module('mean101', ['ngRoute'])
+	.config($routeProvider =>
+		$routeProvider
+			.when('/', {
+				controller: 'MainCtrl',
+				templateUrl: 'partials/main.html'
+			})
+			.when('/chat', {
+				controller: 'ChatCtrl',
+				templateUrl: 'partials/chat.html'
+			})
+	)
+	.controller('MainCtrl', function ($scope, $http) {
 		$http.get('/api/title')
 		.then(({data: { title }}) =>
 			$scope.title = title)
+	})
+	.controller('ChatCtrl', function ($scope, $http) {
+		$http.get('/api/messages')
+		.then(({data: { messages }}) =>
+			$scope.messages = messages)
 	})
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 
+const app = express()
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanchat'
 const port = process.env.PORT || 3000
 
 
@@ -12,8 +14,25 @@ app.use(express.static('client'))
 
 //Gets
 app.get('/api/title', (req, res) =>
-	res.json({"title": 'MEAN 101 from Node' })
+	res.json({"title": 'MEAN Chat' })
 )
 
-//List
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+const Message = mongoose.model('message', {
+	author: String,
+	content: String
+})
+
+
+app.get('/api/messages', (req, res, err) =>
+	Message
+	.find()
+	.then(messages => res.json({messages}))
+	.catch(err)
+)
+
+//Listen
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () =>
+	app.listen(port, () => console.log(`Listening on port: ${port}`))
+	)
+
